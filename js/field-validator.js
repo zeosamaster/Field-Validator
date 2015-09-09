c72baa921934f1947d5a1ebdc064929cf6e85cef
Fabio Matias <fabio_mcm2@hotmail.com>
2015-09-09 14:56:55 +0100

    initial commit

---
+++ js/field-validator.js
@@ -0,0 +1,306 @@
+/*global alertify:false, $:false, console:false */
+
+(function () {
+	'use strict';
+
+	function FieldValidator() {
+
+		/**********************/
+		/* private properties */
+		/**********************/
+
+		var
+			validator = {},
+			validations = {},
+			$container,
+
+			// constants
+			names = {
+				// tooltips
+				tooltip_class: "error-input-tooltip",
+				tooltip_custom_class: "",
+				tooltip_message_attr: "data-tooltip-html",
+				tooltip_added_handler: "tooltip-added"
+			},
+
+			// callbacks
+			after_validation_callback,
+
+			// alertify
+			alertify_defaults = {
+				error_delay: 10,
+				warning_delay: 5
+			},
+			$alertify = alertify;
+
+		/*********************/
+		/* private functions */
+		/*********************/
+
+		// alertify functions
+
+		function alertify_message(message, delay) {
+			delay = delay || alertify_defaults.warning_delay;
+			$alertify.message(message, delay);
+		}
+
+		function alertify_error(error, delay) {
+			delay = delay || alertify_defaults.error_delay;
+			$alertify.message(error, delay);
+		}
+
+		// utils
+
+		function getElemFieldAttrs(elem) {
+			var options = {};
+			$.each(elem[0].attributes, function (index, attr) {
+				if (attr && attr.name && attr.name.indexOf("data-field-") > -1) {
+					options[attr.name] = attr.value;
+				}
+			});
+			return options;
+		}
+
+		function getValidationTypes(input) {
+			var validation_types = (typeof (input) === "string" ? input : $(input).attr("data-field-validations"));
+			if (validation_types.length) {
+				return validation_types.toLowerCase().split(/[,;\s]+/g);
+			} else {
+				return [];
+			}
+		}
+
+		function replaceAll(text, options) {
+			var prop, regex, field;
+			for (prop in options) {
+				if (options.hasOwnProperty(prop)) {
+					field = options[prop];
+					regex = new RegExp("#" + prop, "g");
+					text = text.replace(regex, field);
+				}
+			}
+			return text;
+		}
+
+		// validation error handling
+
+		function processMessages(elem, messages) {
+			var options = getElemFieldAttrs(elem),
+				i;
+			for (i = 0; i < messages.length; i += 1) {
+				messages[i] = replaceAll(messages[i], options);
+			}
+			return messages.filter(function (item, pos, self) {
+				return self.indexOf(item) === pos;
+			});
+		}
+
+		function handleElemErrors(elem, messages) {
+			if (messages.length) {
+				var message = "";
+				if (elem.attr(names.tooltip_message_attr)) {
+					message = elem.attr(names.tooltip_message_attr);
+				} else if (messages.length > 1) {
+					message = "<span>O campo " + elem.attr("data-field-label") + ":</span>";
+					message += "<ul><li>";
+					message += messages.join("</li><li>");
+					message += "</li></ul>";
+				} else if (messages.length === 1) {
+					message = "<p>O campo " + elem.attr("data-field-label") + " " + messages[0] + "</p>";
+				}
+
+				elem.attr(names.tooltip_message_attr, message)
+					.addClass("error-input-highlight")
+					.trigger(names.tooltip_added_handler + ".field-validator");
+			} else {
+				elem.attr(names.tooltip_message_attr, "")
+					.removeClass("error-input-highlight")
+					.trigger("tooltip-removed.field-validator");
+			}
+		}
+
+		/********************/
+		/* public functions */
+		/********************/
+
+		validator.validate = function (elems) {
+			var valid = true;
+
+			elems = (elems && elems.length ? elems : $('[data-field-validations]', $container));
+			elems.each(function (i, el) {
+				var $elem = $(el),
+					label = $elem.attr("data-field-label"),
+					types = [],
+					required = $elem.attr("data-field-required"),
+					regex = $elem.attr("data-field-regex"),
+					min = $elem.attr("data-field-min-length"),
+					max = $elem.attr("data-field-max-length"),
+					html_types = getValidationTypes($elem),
+					messages = [],
+					$valid = true;
+
+				if (!label) {
+					label = $('label[for=' + $elem.attr('id') + ']', $container).text() || "";
+					$elem.attr("data-field-label", label);
+				}
+
+				// add validation types from independent attributes
+				if (required) {
+					types.push("required");
+				}
+				if (regex) {
+					types.push("regex");
+				}
+				if (min || max) {
+					types.push("length");
+				}
+
+				html_types.forEach(function (type) {
+					if (types.indexOf(type) < 0) {
+						types.push(type);
+					}
+				});
+
+				// validate input according to its validation types
+				types.forEach(function (type) {
+					var type_messages = validations[type].validation_function($elem);
+					if (type_messages.length) {
+						$valid = false;
+						messages = messages.concat(type_messages);
+					}
+				});
+
+				// handle validation errors
+				messages = processMessages($elem, messages);
+				handleElemErrors($elem, messages);
+
+				valid = valid && $valid;
+			});
+
+			if (typeof (after_validation_callback) === "function") {
+				after_validation_callback(valid, elems);
+			}
+		};
+
+		validator.setup = function (options) {
+			// load validation rules and messages
+			$.getScript("js/validations-config.js", function (data) {
+				validations = window.validations;
+				delete window.validations;
+				alertify_message("Setup completed");
+			});
+
+			// remove handlers from previous container
+			if ($container && $container.length) {
+				$container
+					.off(".field-validator")
+					.tooltip("destroy");
+			}
+
+			// set container
+			if (options.container) {
+				$container = $(options.container);
+			} else {
+				$container = ($container.length ? $container : $("body"));
+			}
+
+			// delegate parent validations to children
+			if (options.delegate_validations) {
+				$('[data-field-validations]', $container).not('input').each(function (i, elem) {
+					var $parent = $(elem),
+						children = $('input', $parent),
+						parent_validations = getValidationTypes($parent),
+						parent_attrs = [],
+						field_attrs = [],
+						attr;
+
+					// merge child validation types with parent validation types
+					children.each(function (index, child) {
+						var $child = $(child),
+							child_validations = getValidationTypes($child),
+							filtered_parent_validations = [],
+							merged_validations = [];
+
+						filtered_parent_validations = parent_validations.filter(function (parent_validation) {
+							return child_validations.indexOf(parent_validation) < 0;
+						});
+						if (!filtered_parent_validations.length) {
+							return true;
+						}
+
+						merged_validations = child_validations.concat(filtered_parent_validations);
+						if (!merged_validations.length) {
+							return true;
+						}
+
+						$child.attr("data-field-validations", merged_validations.join(" "));
+					});
+					$parent.removeAttr("data-field-validations");
+
+					// fill child validation attributes with parent validation attributes when empty,
+					parent_attrs = getElemFieldAttrs($parent);
+					for (attr in parent_attrs) {
+						if (parent_attrs.hasOwnProperty(attr)) {
+							field_attrs.push({
+								name: attr,
+								value: parent_attrs[attr]
+							});
+						}
+					}
+					field_attrs.forEach(function (attr) {
+						$(children).not('[' + attr.name + ']').each(function (index, child) {
+							$(child).attr(attr.name, attr.value);
+						});
+						$parent.removeAttr(attr.name);
+					});
+				});
+			}
+
+			// add change handler
+			if (options.on_change) {
+				$container.on("change.field-validator", '[data-field-validations]', function () {
+					validator.validate($(this));
+				});
+			}
+
+			// add validation callback
+			if (typeof (options.after_validation_callback) === "function") {
+				after_validation_callback = options.after_validation_callback;
+			}
+
+			// initialize tooltip
+			if (options.tooltip) {
+				names.tooltip_class = options.tooltip.tooltip_class || names.tooltip_class;
+				$container.tooltip({
+					items: ".error-input-highlight",
+					tooltip_class: names.tooltip_class,
+					content: function () {
+						return $(this).attr("data-tooltip-html");
+					}
+				});
+			}
+		};
+
+		return validator;
+	}
+
+	/***************/
+	/* instantiate */
+	/***************/
+
+	$(document).ready(function () {
+		var validator = new FieldValidator();
+		validator.setup({
+			container: "body",
+			delegate_validations: true,
+			on_change: true,
+			tooltip: true,
+			after_validation_callback: function () {
+				console.log("after_validation_callback");
+			}
+		});
+
+		window.validator = validator;
+	});
+
+}());
---
+++ js/validations-config.js
@@ -0,0 +1,282 @@
+(function () {
+	'use strict';
+
+	var validations = {
+		required: {
+			message: "é de preenchimento obrigatório",
+			validation_function: function ($elem) {
+				var value = $elem.val(),
+					messages = [];
+
+				if (!value.length) {
+					messages.push(this.message);
+				}
+				return messages;
+			}
+		},
+
+		regex: {
+			message: "contém caracteres inválidos<br/>Expressão de validação: #data-field-regex",
+			validation_function: function ($elem) {
+				var value = $elem.val(),
+					regex = $elem.attr("data-field-regex"),
+					messages = [];
+
+				if (regex && !regex.test(value)) {
+					messages.push(this.message);
+				}
+				return messages;
+			}
+		},
+
+		min_length: {
+			message: "deve ter no mínimo #data-field-min-length caracteres",
+			validation_function: function ($elem) {
+				var value = $elem.val(),
+					min_length = $elem.attr("data-field-min-length"),
+					messages = [];
+
+				if (min_length && (!value || value.length < min_length)) {
+					messages.push(this.message);
+				}
+				return messages;
+			}
+		},
+
+		max_length: {
+			message: "deve ter no máximo #data-field-max-length caracteres",
+			validation_function: function ($elem) {
+				var value = $elem.val(),
+					max_length = $elem.attr("data-field-max-length"),
+					messages = [];
+
+				if (max_length && (!value || value.length > max_length)) {
+					messages.push(this.message);
+				}
+				return messages;
+			}
+		},
+
+		no_spaces: {
+			message: "não deve ter espaços",
+			validation_function: function ($elem) {
+				var value = $elem.val(),
+					messages = [];
+
+				if (/\s/g.test(value)) { // value has spaces
+					messages.push(this.message);
+				}
+				return messages;
+			}
+		},
+
+		positive: {
+			message: "deve conter um número positivo",
+			validation_function: function ($elem) {
+				var value = $elem.val(),
+					aux_value = parseFloat(value),
+					messages = [];
+
+				if (aux_value < 0) { // value is not positive
+					messages.push(this.message);
+				}
+				messages = messages.concat(validations.no_spaces.validation_function($elem));
+				return messages;
+			}
+		},
+
+		upper: {
+			message: "deve conter apenas letras maiúsculas",
+			validation_function: function ($elem) {
+				var value = $elem.val(),
+					messages = [];
+
+				if (value.toUpperCase() !== value) {
+					messages.push(this.message);
+				}
+				return messages;
+			}
+		},
+
+		lower: {
+			message: "deve conter apenas letras minúsculas",
+			validation_function: function ($elem) {
+				var value = $elem.val(),
+					messages = [];
+
+				if (value.toLowerCase() !== value) {
+					messages.push(this.message);
+				}
+				return messages;
+			}
+		},
+
+		digits: {
+			message: "deve conter apenas algarismos de 0 a 9",
+			validation_function: function ($elem) {
+				var value = $elem.val(),
+					messages = [];
+
+				if (!/^[0-9]*$/.test(value)) {
+					messages.push(this.message);
+				}
+				return messages;
+			}
+		},
+
+		required_digit: {
+			message: "deve conter pelo menos um dígito",
+			validation_function: function ($elem) {
+				var value = $elem.val(),
+					messages = [];
+
+				if (!/\d+/.test(value)) {
+					messages.push(this.message);
+				}
+				return messages;
+			}
+		},
+
+		required_lower: {
+			message: "deve conter pelo menos um caracter minúsculo",
+			validation_function: function ($elem) {
+				var value = $elem.val(),
+					messages = [];
+
+				if (!/[a-z]+/.test(value)) {
+					messages.push(this.message);
+				}
+				return messages;
+			}
+		},
+
+		required_upper: {
+			message: "deve conter pelo menos um caracter maiúsculo",
+			validation_function: function ($elem) {
+				var value = $elem.val(),
+					messages = [];
+
+				if (!/[A-Z]+/.test(value)) {
+					messages.push(this.message);
+				}
+				return messages;
+			}
+		},
+
+		text: {
+			message: "deve conter apenas espaços, letras de A a Z (maiúsculas ou minúsculas) e algarismos de 0 a 9",
+			validation_function: function ($elem) {
+				var value = $elem.val(),
+					messages = [];
+
+				if (!/^[a-zA-Z0-9\s]*$/.test(value)) { // value doesn't contain only valid characters
+					messages.push(this.message);
+				}
+				return messages;
+			}
+		},
+
+		email: {
+			message: "deve ter o formato email@email.com",
+			validation_function: function ($elem) {
+				var value = $elem.val(),
+					messages = [];
+
+				if (!/[a-z0-9!#$%&\'*+\/=?\^_`{|}~\-]+(?:\.[a-z0-9!#$%&\'*+\/=?\^_`{|}~\-]+)*@(?:[a-z0-9](?:[a-z0-9\-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9\-]*[a-z0-9])?/i.test(value)) {
+					messages.push(this.message);
+				}
+				return messages;
+			}
+		},
+
+		integer: {
+			not_integer_string_message: "deve conter um número inteiro",
+			not_number_message: "deve conter um número inteiro",
+			not_integer_message: "deve conter um número inteiro",
+			validation_function: function ($elem) {
+				var value = $elem.val(),
+					aux_value = parseFloat(value),
+					no_spaces = validations.no_spaces.validation_function($elem),
+					messages = [];
+
+				if (!/^[+\-]?\d+((\.\d+)?e(\+)?\d+)?$/i.test(value.toString())) { // value is not a integer string
+					messages.push(this.not_integer_string_message);
+				}
+				if (typeof aux_value !== "number") { // value is not of type number (includes NaN)
+					messages.push(this.not_number_message);
+				}
+				if (aux_value % 1 !== 0) { // value is not integer (NaN isn't)
+					messages.push(this.not_integer_message);
+				}
+				if (no_spaces.length) {
+					messages = messages.concat(no_spaces);
+				}
+				return messages;
+			}
+		},
+
+		decimal: {
+			not_finite_message: "deve conter um número finito",
+			not_a_float_message: "deve conter um valor decimal",
+			not_a_number_message: "deve conter um valor decimal",
+			validation_function: function ($elem) {
+				var value = $elem.val(),
+					no_spaces = validations.no_spaces.validation_function($elem),
+					messages = [];
+
+				if (!isFinite(value)) { // value is finite
+					messages.push(this.not_finite_message);
+				}
+				if (!/^[+\-]?\d*\.?\d+(e[+\-]?\d+)?$/i.test(value)) { // value is a valid float
+					messages.push(this.not_a_float_message);
+				}
+				if (isNaN(parseFloat(value))) { // value is a number
+					messages.push(this.not_a_number_message);
+				}
+				if (no_spaces.length) {
+					messages = messages.concat(no_spaces);
+				}
+				return messages;
+			}
+		},
+
+		length: {
+			validation_function: function ($elem) {
+				var min_length = validations.min_length.validation_function($elem),
+					max_length = validations.max_length.validation_function($elem),
+					messages = [];
+
+				if (min_length) {
+					messages = messages.concat(min_length);
+				}
+				if (max_length) {
+					messages = messages.concat(max_length);
+				}
+				return messages;
+			}
+		},
+
+		password: {
+			validation_function: function ($elem) {
+				var digit = validations.required_digit.validation_function($elem),
+					lower = validations.required_lower.validation_function($elem),
+					upper = validations.required_upper.validation_function($elem),
+					messages = [];
+
+				if (digit.length) {
+					messages = messages.concat(digit);
+				}
+				if (lower.length) {
+					messages = messages.concat(lower);
+				}
+				if (upper.length) {
+					messages = messages.concat(upper);
+				}
+				return messages;
+			}
+		}
+	};
+
+	window.validations = validations;
+
+}());
